5f5e5471a9da4c73d14f7648420205be40635d97
park <pjh2196@gmail.com>
2026-01-21 14:57:20 +0900

    Initial Commit

---
+++ MiniMyizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniMyizer
+//
+//  Created by パク・ジホ on 2026/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniMyizer/MiniMyizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMyizerApp.swift
+//  MiniMyizer
+//
+//  Created by パク・ジホ on 2026/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MiniMyizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
